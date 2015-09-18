2e9d3d957874a20bc9fbe2d3911e953dbd7f0e34
mateuspelegrino <mateuspelegrino@gmail.com>
2015-09-18 20:31:37 -0300

    template inicial da tela principal

---
+++ Projeto/Controllers/telaPrincipal.js
@@ -0,0 +1,10 @@
+/**
+ * Created by MateusPelegrino on 18/09/15.
+ */
+
+var app = angular.module('telaprincipal', [
+    'jcs-autoValidate',
+    'angular-ladda'
+]);
+app.controller('TelaPrincipalCtrl', function ($scope) {
+});
